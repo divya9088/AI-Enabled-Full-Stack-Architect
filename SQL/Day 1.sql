3e05f6f9db4b44761143fdb195b4f5068f33d4bc
Divyajyoti Saha <divyajyoti9088@gmail.com>
2025-08-09 17:33:51 +0530

    push

--- SQL/Day 1.sql
+++ SQL/Day 1.sql
@@ -2,9 +2,11 @@ CREATE DATABASE Day1DB;
 
 use Day1DB;
 
-CREATE TABLE Students ( Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(100), Age INT )
+--CREATE TABLE Students ( Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(100), Age INT )
 
 INSERT INTO Students (Name, Age) VALUES ('Alice', 22), ('Bob', 25);
 
 SELECT * FROM Students;
 
+ ALTER TABLE Students ADD Marks INT; 
+ INSERT INTO Students (Name, Age, Marks) VALUES ('John', 24, 88);
